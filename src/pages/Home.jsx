01fcf60025fc5d93f3bc19c5e0ea8741fcc1cd97
Onwuka Leroy <jinxpc8008@gmail.com>
2025-07-10 12:42:36 +0100

    fixed some errors

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,19 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Carousel from "../Components/Carousel";
-<<<<<<< HEAD
 import Stats from "../Components/Stats";
-=======
 import ReservationForm from "../Components/ReservationForm";
->>>>>>> a25dfb3140c922267e32153e913ff2894c3bf02a
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Carousel />
-<<<<<<< HEAD
-        <Stats />
+      <Stats />
+      <ReservationForm />
+
    
       
-=======
-      <ReservationForm />
-      <h1>Hello World</h1>
->>>>>>> a25dfb3140c922267e32153e913ff2894c3bf02a
     </>
   );
 };
