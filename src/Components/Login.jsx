22b87963c6ea1b74a246cc199adf3c201c7116c3
Onwuka Leroy <jinxpc8008@gmail.com>
2025-09-05 09:46:38 +0100

    fixed redirecting issue

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: "https://epicure-reserve.netlify.app",
         },
       });
 
